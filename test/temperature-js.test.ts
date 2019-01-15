e33c96fafc397942e8412665a040590db38a4784
Angela Melinda <angela.melinda@shopee.com>
2019-01-15 13:49:28 +0700

    test(temperature-js): remove unnecessary test

--- test/temperature-js.test.ts
+++ test/temperature-js.test.ts
@@ -4,10 +4,6 @@ import Temperature from '../src/temperature-js'
  * TemperatureTest
  */
 describe('TemperatureTest', () => {
-  it('works if true is truthy', () => {
-    expect(true).toBeTruthy()
-  })
-
   it('convert celcius to kelvin', () => {
     expect(Temperature.celciusToKelvin(10)).toBe(283.15)
   })
